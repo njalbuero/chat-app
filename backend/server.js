4ba2992a6eaab1dc503efba770431dcc0c051633
njalbuero <njalbuero@gmail.com>
2024-09-06 14:41:19 +0800

    Use userRoutes

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ import cookieParser from "cookie-parser";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
+import userRoutes from "./routes/user.routes.js";
 import connectToMongoDB from "./db/connectToMongoDB.js";
 
 dotenv.config();
@@ -15,7 +16,8 @@ app.use(express.json()); // for parsing application/json from request body
 app.use(cookieParser()); // for parsing cookie from request
 
 app.use("/api/auth", authRoutes);
-app.use("/api/messages/", messageRoutes)
+app.use("/api/messages/", messageRoutes);
+app.use("/api/users/", userRoutes);
 
 app.listen(PORT, () => {
   connectToMongoDB();
