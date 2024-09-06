57e545849d3e85e55f60dd018c0ec07ba105ae09
njalbuero <njalbuero@gmail.com>
2024-09-06 14:41:07 +0800

    Create file

---
+++ backend/controllers/user.controller.js
@@ -0,0 +1,17 @@
+import User from "../models/user.model.js";
+
+export const getUsersForSidebar = async (req, res) => {
+  try {
+    const userId = req.user._id;
+    const filteredUsers = await User.find({
+      _id: { $ne: userId },
+    }).select("-password");
+
+    res.status(200).json(filteredUsers);
+  } catch (error) {
+    console.log("Error in get users for sidebar controller", error.message);
+    res.status(500).json({
+      error: "Internal server error",
+    });
+  }
+};
