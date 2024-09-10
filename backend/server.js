8a4d82d227920a89f53ea0c409d33641e4a6bdc3
njalbuero <njalbuero@gmail.com>
2024-09-11 03:01:57 +0800

    Modify for deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
+import path from "path";
 
 import authRoutes from "./routes/auth.routes.js";
 import messageRoutes from "./routes/message.routes.js";
@@ -12,6 +13,7 @@ import { app, server } from "./socket/socket.js";
 dotenv.config();
 
 const PORT = process.env.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json()); // for parsing application/json from request body
 app.use(cookieParser()); // for parsing cookie from request
@@ -20,7 +22,14 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages/", messageRoutes);
 app.use("/api/users/", userRoutes);
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+
 server.listen(PORT, () => {
+  console.log('adad', __dirname);
   connectToMongoDB();
   console.log(`Server running on port ${PORT}`);
 });
