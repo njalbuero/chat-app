a29379a0a9a716a48fd70b569992cb70c0db1550
Nhielsan Albuero <njalbuero@gmail.com>
2024-10-06 21:04:13 +0800

    Change route and controller

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,15 +1,42 @@
-import User from "../models/user.model.js";
+import Conversation from "../models/conversation.model.js";
+import mongoose from "mongoose";
 
-export const getUsersForSidebar = async (req, res) => {
+export const getConversations = async (req, res) => {
   try {
-    const userId = req.user._id;
-    const filteredUsers = await User.find({
-      _id: { $ne: userId },
-    }).select("-password");
+    const userId = req.params.userId;
+
+    const conversations = await Conversation.aggregate([
+      {
+        $match: {
+          participants: mongoose.Types.ObjectId(userId), // Match the userId in participants array
+        },
+      },
+      {
+        $lookup: {
+          from: "messages", // The name of your messages collection
+          localField: "messages",
+          foreignField: "_id",
+          as: "messagesDetails",
+        },
+      },
+      {
+        $project: {
+          participants: 1,
+          messages: 1,
+          lastMessage: { $arrayElemAt: ["$messagesDetails", -1] }, // Get the last message document
+        },
+      },
+    ]);
+
+    if (!conversations || conversations.length === 0) {
+      return res.status(404).json({ message: "No conversations found" });
+    }
+
+    res.json(conversations);
 
     return res.status(200).json(filteredUsers);
   } catch (error) {
-    console.log("Error in get users for sidebar controller", error.message);
+    console.log("Error in get conversations controller", error.message);
     return res.status(500).json({
       error: "Internal server error",
     });
--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,9 @@
 import express from "express";
-import { getUsersForSidebar } from "../controllers/user.controller.js";
+import { getConversations } from "../controllers/user.controller.js";
 import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
-router.get("/", protectRoute, getUsersForSidebar);
+router.get("/:id/conversations", protectRoute, getConversations);
 
 export default router;
