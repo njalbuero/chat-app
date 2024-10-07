1c725039fcd87fcf1a3c68e9212b6b4a1243d2b9
njalbuero <njalbuero@gmail.com>
2024-10-08 02:23:47 +0800

    Remove hook

--- frontend/src/zustand/useSearchConversation.js
+++
@@ -1,8 +0,0 @@
-import { create } from "zustand";
-
-const useSearchConversation = create((set) => ({
-	search: "",
-	setSearch: (search) => set({ search }),
-}));
-
-export default useSearchConversation;
