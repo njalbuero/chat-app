4e86c06ec0f04c77e30a55a0df37c62e039b9649
njalbuero <njalbuero@gmail.com>
2024-10-08 02:22:38 +0800

    Modify getConversations(), add searchUsers()

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,42 +1,71 @@
 import Conversation from "../models/conversation.model.js";
-import mongoose from "mongoose";
+import User from "../models/user.model.js";
 
 export const getConversations = async (req, res) => {
   try {
-    const userId = req.params.userId;
+    const userId = req.params.id;
 
-    const conversations = await Conversation.aggregate([
-      {
-        $match: {
-          participants: mongoose.Types.ObjectId(userId), // Match the userId in participants array
-        },
-      },
-      {
-        $lookup: {
-          from: "messages", // The name of your messages collection
-          localField: "messages",
-          foreignField: "_id",
-          as: "messagesDetails",
-        },
-      },
-      {
-        $project: {
-          participants: 1,
-          messages: 1,
-          lastMessage: { $arrayElemAt: ["$messagesDetails", -1] }, // Get the last message document
-        },
-      },
-    ]);
+    let conversations = await Conversation.find({
+      participants: userId,
+    })
+      .populate({
+        path: "messages",
+        limit: 1,
+        populate: { path: "senderId" },
+      })
+      .populate({ path: "participants", match: { _id: { $ne: userId } } });
+
+      console.log(conversations)
 
     if (!conversations || conversations.length === 0) {
-      return res.status(404).json({ message: "No conversations found" });
+      return res.status(200).json([]);
     }
 
-    res.json(conversations);
+    conversations = conversations.map((conversation) => {
+      // Ensure there is at least one message
+      const lastMessage = conversation.messages[0];
 
-    return res.status(200).json(filteredUsers);
+      return {
+        _id: conversation._id,
+        recipient: conversation.participants[0], // the user they are conversing with
+        lastMessage: lastMessage
+          ? {
+              message: lastMessage.message,
+              sender: lastMessage.senderId,
+              createdAt: lastMessage.createdAt,
+            }
+          : null, // Handle the case where there are no messages
+      };
+    });
+
+    // Sort by lastMessage.createdAt (latest first)
+    conversations.sort((a, b) => {
+      if (a.lastMessage && b.lastMessage) {
+        return (
+          new Date(b.lastMessage.createdAt) - new Date(a.lastMessage.createdAt)
+        );
+      }
+      return 0; // Handle cases where there are no messages
+    });
+
+    return res.status(200).json(conversations);
   } catch (error) {
     console.log("Error in get conversations controller", error.message);
+    return res.status(500).json({
+      error: "Internal server error",
+    });
+  }
+};
+
+export const searchUsers = async (req, res) => {
+  try {
+    const { name } = req.query;
+    const users = await User.find({
+      fullName: { $regex: name, $options: "i" },
+    }).sort({ fullName: 1 });
+    return res.status(200).json(users);
+  } catch (error) {
+    console.log("Error in search users controller", error.message);
     return res.status(500).json({
       error: "Internal server error",
     });
