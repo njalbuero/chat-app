74fd67d931db7690f0f05b16bca9efbdb2489919
njalbuero <njalbuero@gmail.com>
2024-10-08 02:22:50 +0800

    Add /search route

--- backend/routes/user.routes.js
+++ backend/routes/user.routes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { getConversations } from "../controllers/user.controller.js";
+import {
+  getConversations,
+  searchUsers,
+} from "../controllers/user.controller.js";
 import protectRoute from "../middleware/protectRoute.js";
 
 const router = express.Router();
 
 router.get("/:id/conversations", protectRoute, getConversations);
+router.get("/search", searchUsers);
 
 export default router;
